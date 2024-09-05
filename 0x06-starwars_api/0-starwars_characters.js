41822d24703f576061f28ddf70b47df335bba724
sw-ouahmane <133534816+sw-ouahmane@users.noreply.github.com>
2024-09-05 20:44:12 +0100

    Add

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,24 @@
 #!/usr/bin/node
+/* prints all characters of star wars movie passed as commandline argument */
 
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-request(url, async function (error, response) {
-    if (error) {
-        console.log(error);
-    } else {
-        if (response.statusCode === 200) {
-            try {
-                const movie = JSON.parse(response.body);
-                const characters = movie.characters;
-                for (const character of characters) {
-                    await getCharacterName(character);
-                }
-            } catch (parseError) {
-                console.error(parseError);
-            }
-        } else {
-            console.log(response.statusCode);
-        }
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, async (err, res, body) => {
+    err && console.log(err);
+
+    const charactersArray = (JSON.parse(res.body).characters);
+    for (const character of charactersArray) {
+        await new Promise((resolve, reject) => {
+            request(character, (err, res, body) => {
+                err && console.log(err);
+
+                console.log(JSON.parse(body).name);
+                resolve();
+            });
+        });
     }
 });
-
-function getCharacterName(url) {
-    return new Promise((resolve, reject) => {
-        request(url, function (error, response) {
-            if (error) {
-                reject(error);
-            } else {
-                if (response.statusCode === 200) {
-                    try {
-                        const character = JSON.parse(response.body);
-                        resolve(console.log(character.name));
-                    } catch (parseError) {
-                        reject(parseError);
-                    }
-                } else {
-                    reject((response.statusCode));
-                }
-            }
-        });
-    });
-}
